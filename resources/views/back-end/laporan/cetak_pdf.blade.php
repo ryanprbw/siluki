9b3873e0bd69eb047811a3d1014bc5b6fcd94f54
Ryan Prabowo, S.T <ryanprabowo96@gmail.com>
2024-10-02 11:07:21 +0800

    updating cetak pdf

--- resources/views/back-end/laporan/cetak_pdf.blade.php
+++ resources/views/back-end/laporan/cetak_pdf.blade.php
@@ -11,8 +11,9 @@
             border-collapse: collapse;
             font-family: Helvetica, sans-serif;
             font-size: 8px;
-            
+
         }
+
         h2 {
             width: 100%;
             border-collapse: collapse;
@@ -25,6 +26,18 @@
             border: 1px solid #000;
             padding: 8px;
             text-align: center;
+
+        }
+
+        td.realisasi {
+            word-wrap: break-word;
+            /* Membungkus kata */
+            word-break: break-all;
+            /* Memecah kata panjang tanpa spasi */
+            white-space: normal;
+            /* Memastikan teks dibungkus */
+            max-width: 50px;
+            /* Tentukan lebar maksimum kolom */
         }
 
         thead {
@@ -43,8 +56,9 @@
 </head>
 
 @foreach ($laporansByBidang as $bidang => $laporans)
-<body>
-    <h1 class="center">Laporan Rekap Sasaran Perangkat Daerah Tahun {{ $tahun }} Kabupaten Tapin</h1>
+
+    <body>
+        <h1 class="center">Laporan Rekap Sasaran Perangkat Daerah Tahun {{ $tahun }} Kabupaten Tapin</h1>
         <h2>Bidang: {{ $bidang }}</h2>
         <table>
             <thead>
@@ -100,70 +114,71 @@
                 @endforeach
             </tbody>
         </table>
-   
 
-    <div class="pagebreak"></div>
-    <h1 class="center">Catatan Laporan Rekap Sasaran Perangkat Daerah Tahun {{ $tahun }} Kabupaten Tapin</h1>
-    <h2>Bidang: {{ $bidang }}</h2>
-    <table>
-        <thead>
-            <tr>
-                <th rowspan="2">No.</th>
-                <th rowspan="2">Sasaran Kinerja</th>
-                <th rowspan="2">Indikator</th>
-                <th rowspan="2">Target</th>
-                <th rowspan="2">Satuan</th>
-                <th colspan="3">Triwulan 1</th>
-                <th colspan="3">Triwulan 2</th>
-                <th colspan="3">Triwulan 3</th>
-                <th colspan="3">Triwulan 4</th>
-                <th rowspan="2">Jabatan</th>
-            </tr>
-            <tr>
-                <th>Realisasi</th>
-                <th>Faktor Pendorong</th>
-                <th>Faktor Penghambat</th>
-                <th>Realisasi</th>
-                <th>Faktor Pendorong</th>
-                <th>Faktor Penghambat</th>
-                <th>Realisasi</th>
-                <th>Faktor Pendorong</th>
-                <th>Faktor Penghambat</th>
-                <th>Realisasi</th>
-                <th>Faktor Pendorong</th>
-                <th>Faktor Penghambat</th>
-            </tr>
-        </thead>
-        <tbody>
-            @forelse ($laporans as $dash)
+
+        <div class="pagebreak"></div>
+        <h1 class="center">Catatan Laporan Rekap Sasaran Perangkat Daerah Tahun {{ $tahun }} Kabupaten Tapin
+        </h1>
+        <h2>Bidang: {{ $bidang }}</h2>
+        <table>
+            <thead>
                 <tr>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $dash->sasaran }}</td>
-                    <td>{{ $dash->indikator }}</td>
-                    <td>{{ $dash->target }}</td>
-                    <td>{{ $dash->satuan }}</td>
-                    <td>{{ $dash->realisasi_ctt_t1 }}</td>
-                    <td>{{ $dash->faktor_pendorong_t1 }}</td>
-                    <td>{{ $dash->faktor_penghambat_t1 }}</td>
-                    <td>{{ $dash->realisasi_ctt_t2 }}</td>
-                    <td>{{ $dash->faktor_pendorong_t2 }}</td>
-                    <td>{{ $dash->faktor_penghambat_t2 }}</td>
-                    <td>{{ $dash->realisasi_ctt_t3 }}</td>
-                    <td>{{ $dash->faktor_pendorong_t3 }}</td>
-                    <td>{{ $dash->faktor_penghambat_t3 }}</td>
-                    <td>{{ $dash->realisasi_ctt_t4 }}</td>
-                    <td>{{ $dash->faktor_pendorong_t4 }}</td>
-                    <td>{{ $dash->faktor_penghambat_t4 }}</td>
-                    <td>{{ $dash->user->jabatan }}</td>
+                    <th rowspan="2">No.</th>
+                    <th rowspan="2">Sasaran Kinerja</th>
+                    <th rowspan="2">Indikator</th>
+                    <th rowspan="2">Target</th>
+                    <th rowspan="2">Satuan</th>
+                    <th colspan="3">Triwulan 1</th>
+                    <th colspan="3">Triwulan 2</th>
+                    <th colspan="3">Triwulan 3</th>
+                    <th colspan="3">Triwulan 4</th>
+                    <th rowspan="2">Jabatan</th>
                 </tr>
-            @empty
                 <tr>
-                    <td colspan="18" class="text-center">Data masih Kosong</td>
+                    <th>Realisasi</th>
+                    <th>Faktor Pendorong</th>
+                    <th>Faktor Penghambat</th>
+                    <th>Realisasi</th>
+                    <th>Faktor Pendorong</th>
+                    <th>Faktor Penghambat</th>
+                    <th>Realisasi</th>
+                    <th>Faktor Pendorong</th>
+                    <th>Faktor Penghambat</th>
+                    <th>Realisasi</th>
+                    <th>Faktor Pendorong</th>
+                    <th>Faktor Penghambat</th>
                 </tr>
-            @endforelse
-        </tbody>
-    </table>
-</body>
+            </thead>
+            <tbody>
+                @forelse ($laporans as $dash)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td class="realisasi">{{ $dash->sasaran }}</td>
+                        <td class="realisasi">{{ $dash->indikator }}</td>
+                        <td>{{ $dash->target }}</td>
+                        <td>{{ $dash->satuan }}</td>
+                        <td class="realisasi">{{ $dash->realisasi_ctt_t1 }}</td>
+                        <td>{{ $dash->faktor_pendorong_t1 }}</td>
+                        <td>{{ $dash->faktor_penghambat_t1 }}</td>
+                        <td class="realisasi">{{ $dash->realisasi_ctt_t2 }}</td>
+                        <td>{{ $dash->faktor_pendorong_t2 }}</td>
+                        <td>{{ $dash->faktor_penghambat_t2 }}</td>
+                        <td class="realisasi">{{ $dash->realisasi_ctt_t3 }}</td>
+                        <td>{{ $dash->faktor_pendorong_t3 }}</td>
+                        <td>{{ $dash->faktor_penghambat_t3 }}</td>
+                        <td class="realisasi">{{ $dash->realisasi_ctt_t4 }}</td>
+                        <td>{{ $dash->faktor_pendorong_t4 }}</td>
+                        <td>{{ $dash->faktor_penghambat_t4 }}</td>
+                        <td>{{ $dash->user->jabatan }}</td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="18" class="text-center">Data masih Kosong</td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </body>
 @endforeach
 
 </html>
